03de499ada8bc30aeff45412cdf80d30fb59ebe4
Paulo Kinjo <paulokinjo@hotmail.com>
2020-03-09 06:32:16 +0900

    Start django pyshop

--- mosh/automation_with_python/app.py
+++ mosh/automation_with_python/app.py
@@ -1,9 +1,28 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
-wb = xl.load_workbook('transactions.xlsx')
-sheet = wb['Sheet1']
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
 
-for row in range(2, sheet.max_row + 1):
-    col_cell = 'c' + str(row)
-    row_cell = sheet[col_cell]
-    print(row_cell.value)
+    print("*" * 15)
+    sheet['d1'].value = "New Price"
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet['c' + str(row)]    
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet['d' + str(row)]    
+        corrected_price_cell.value = corrected_price        
+        
+    values = Reference(sheet, 
+                    min_row=2, 
+                    max_row=sheet.max_row,
+                    min_col=4,
+                    max_col=4)
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+
+    wb.save(filename)
+
+process_workbook('transactions.xlsx')

a8482b0620ccf54e88d73a5b0cfef8722ef0f691
Paulo Kinjo <paulokinjo@hotmail.com>
2020-03-15 21:09:29 +0900

    Extract data

--- python_automation_cookbook/extract_data.py
+++ python_automation_cookbook/extract_data.py
@@ -1,13 +1,33 @@
 import delorean
 from decimal import Decimal
+from parse import parse
 
-log = '[2018-05-05T11:07:12.267897] - SALE - PRODUCT: 1345 - PRICE: $09.99'
+class PriceLog(object):
+    def __init__(self, timestamp, product_id, price):
+        self.timestamp = timestamp
+        self.product_id = product_id
+        self.price = price
+    
+    def __repr__(self):
+        return '<PriceLog({}, {}, {}>'.format(self.timestamp, self.product_id, self.price)
+    
+    @classmethod
+    def parse(cls, text_log):
+        '''
+        Parse from a text log with the format
+        [<Timestamp>] - SALE - PRODUCT: <product id> - PRICE: $<price>
+        to a PriceLog object
+        '''
+        def price(string):
+            return Decimal(string)
+        
+        def isodate(string):
+            return delorean.parse(string)
+        
+        FORMAT = ('[{timestamp:isodate}] - SALE - PRODUCT: {product:d} - PRICE: ${price:price}')
+        formats = {'price': price, 'isodate': isodate}
+        result = parse(FORMAT, text_log, formats)
+        return cls(timestamp=result['timestamp'], product_id=result['product'], price=result['price'])
 
-divide_it = log.split(' - ')
-timestamp_string, _, product_string, price_string = divide_it
-
-timestamp = delorean.parse(timestamp_string.strip('[]'))
-product_id = int(product_string.split(':')[-1])
-price = Decimal(price_string.split('$')[-1])
-
-print(timestamp, product_id, price)
+log = '[2018-05-06T14:58:59.051545] - SALE - PRODUCT: 827 - PRICE: $22.25'
+print(PriceLog.parse(log))

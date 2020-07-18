4dd64ca4197a639db63d125db181737d5eaad44a
Paulo Kinjo <paulokinjo@hotmail.com>
2020-07-18 21:55:33 +0900

    Add adt

---
+++ adt/checkdates.py
@@ -0,0 +1,24 @@
+from date import Date
+
+
+def main():
+    bornBefore = Date(6, 1, 1988)
+    date = promptAndExtractDate()
+    while date is not None:
+        if date <= bornBefore:
+            print("Is at least " + str(bornBefore) + " years of age: ", date)
+        date = promptAndExtractDate()
+
+
+def promptAndExtractDate():
+    print("Enter a birth date.")
+    month = int(input("month (0 to quit): "))
+    if(month == 0):
+        return None
+    else:
+        day = int(input("day: "))
+        year = int(input("year: "))
+        return Date(month, day, year)
+
+
+main()

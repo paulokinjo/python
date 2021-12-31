15fa65f599a9a03ba1256bdbc1abc326f6d29d3b
Paulo Kinjo <paulokinjo@hotmail.com>
2021-12-31 10:10:27 +0900

    First FT specced out in comments and now uses unittest

--- tdd/goat/functional_tests.py
+++ tdd/goat/functional_tests.py
@@ -2,11 +2,51 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import unittest
 
 
-service=Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
-driver.maximize_window()
-driver.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'The install worked successfully!' in driver.title
+    def __init__(self, *args, **kwargs):
+        super(NewVisitorTest, self).__init__(*args, **kwargs)
+        self.service = Service(ChromeDriverManager().install())
+
+    def setUp(self):
+        self.browser = webdriver.Chrome(service=self.service)        
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+        
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # she is invited to enter a to-do item straight away
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+
+        # when she hits enter, the page updates, and now the page lists
+        # "1: Buy a peacock feathers" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+        # The page updates again, and now shows both items on her list
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
+
+
+if __name__ == '__main__':
+    unittest.main()
